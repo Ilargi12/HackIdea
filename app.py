46513a9ff9da39deed6e433c127f25767ce5087b
Frank <bplewnia99@gmail.com>
2021-03-28 10:41:08 +0200

    Return simple data from /results endpoint

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
+@app.route('/results')
 def hello_world():
-    return 'Hello World!'
+    return {'name':['hi macarena', 'hi chichua'], 'description': ['lozl', 'kimbs']}
 
 
 if __name__ == '__main__':
